991a2e3aaa4741ec8c44efa545c1079b08f9e27d
Frank Mullenger <frank.mullenger@kumutu.com>
2010-10-10 22:09:02 +1300

    Moving timezone setting to module config.

--- _config.php
+++ _config.php
@@ -1,4 +1,7 @@
 <?php
 Object::add_extension('Member', 'AppointmentBuyer');
 Object::add_extension('Conference', 'Appointment');
+
+//Set timezone
+date_default_timezone_set('Pacific/Auckland');
 ?>
